558e49ce31b8d885bbcdf0e2a77d2190bda50f7f
Yucan Liu <yucan@mac.lan>
2025-02-25 21:14:50 -0500

    test github workflow

--- next.config.js
+++ next.config.js
@@ -2,6 +2,9 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  output: 'export',
+  basePath: '/safe.io',
+  assetPrefix: '/safe.io/',
   images: {
     unoptimized: true,  // Required for static export
   },
